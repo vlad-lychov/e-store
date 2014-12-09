78643fefd040bbafacada0148df8615d5fcc89a3
superloboda <ann.loboda@gmail.com>
2014-12-09 21:58:51 +0200

    add User and UserDao

--- estore/src/main/java/com/estore/catalog/datamodel/User.java
+++ estore/src/main/java/com/estore/catalog/datamodel/User.java
@@ -2,4 +2,94 @@ package com.estore.catalog.datamodel;
 
 public class User {
 
+	private String id;
+	private String email;
+	private String name;
+	private String password;
+	private String companyName;
+	private String address;
+	private String country;
+	private String city;
+	private String countryCode;
+	private String phoneNumber;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
---
+++ estore/src/main/java/com/estore/dao/impl/UserDao.java
@@ -0,0 +1,7 @@
+package com.estore.dao.impl;
+
+import com.estore.catalog.datamodel.User;
+
+public class UserDao extends GenericDaoHibernate<String, User>{
+
+}
